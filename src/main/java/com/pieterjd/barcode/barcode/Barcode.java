4b9f3f24bf00edc7afb6839c97249769337fa0e6
Pieter-Jan Drouillon <drouillon@hotmail.com>
2024-01-02 10:18:11 +0100

    align equals/hashcode methods.

--- src/main/java/com/pieterjd/barcode/barcode/Barcode.java
+++ src/main/java/com/pieterjd/barcode/barcode/Barcode.java
@@ -8,6 +8,7 @@ import org.springframework.data.relational.core.mapping.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -20,36 +21,12 @@ import lombok.ToString;
 
 
 @Table("BARCODE")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Barcode {
     @Id
     private Long id;    
+    @EqualsAndHashCode.Include
     private String barcode;
     @Builder.Default
     private Set<Description> descriptions = new HashSet<>();
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((barcode == null) ? 0 : barcode.hashCode());
-        result = prime * result + ((descriptions == null) ? 0 : descriptions.hashCode());
-        return result;
-    }
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Barcode other = (Barcode) obj;
-        if (barcode == null) {
-            if (other.barcode != null)
-                return false;
-        } else if (!barcode.equals(other.barcode))
-            return false;
-        return true;
-    }
-
-    
 }
--- src/test/java/com/pieterjd/barcode/barcode/BarcodeConverterTests.java
+++ src/test/java/com/pieterjd/barcode/barcode/BarcodeConverterTests.java
@@ -3,6 +3,8 @@ package com.pieterjd.barcode.barcode;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,16 +12,34 @@ public class BarcodeConverterTests {
 
     private DescriptionConverter descriptionConverter = new DescriptionConverter();
     private BarcodeConverter barcodeConverter = new BarcodeConverter(descriptionConverter);
+
     @Test
-    void testConvertBarcodeWithout() {
+    void testConvertBarcodeWithoutDescriptions() {
         Barcode b = Barcode.builder()
-        .barcode("123")
-        .descriptions(new HashSet<>())
-        .build();
+                .barcode("123")
+                .descriptions(new HashSet<>())
+                .build();
         BarcodeDto expected = BarcodeDto.builder()
-        .barcode("123")
-        .descriptions(new HashSet<>())
-        .build();
+                .barcode("123")
+                .descriptions(new HashSet<>())
+                .build();
+        BarcodeDto actual = barcodeConverter.convert(b);
+        assertTrue(actual.equals(expected));
+    }
+
+    @Test
+    void testConvertBarcodeWithDescriptions() {
+        Barcode b = Barcode.builder()
+                .barcode("123")
+                .descriptions(
+                        Set.of(
+                                Description.builder().locale(Locale.ENGLISH).text("english").build()))
+                .build();
+        BarcodeDto expected = BarcodeDto.builder()
+                .barcode("123")
+                .descriptions(Set.of(
+                                DescriptionDto.builder().locale("en").text("english text").build()))
+                .build();
         BarcodeDto actual = barcodeConverter.convert(b);
         assertTrue(actual.equals(expected));
     }
