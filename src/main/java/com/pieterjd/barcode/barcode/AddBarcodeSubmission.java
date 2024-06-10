434822bc38440a1340967e48214de0a0c2b1eef6
Pieter-Jan Drouillon <drouillon@hotmail.com>
2024-06-10 19:58:06 +0200

    add barcode form

---
+++ src/main/java/com/pieterjd/barcode/barcode/AddBarcodeSubmission.java
@@ -0,0 +1,19 @@
+package com.pieterjd.barcode.barcode;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddBarcodeSubmission {
+    @NotEmpty
+    @Size(min = 10)
+    private String barcode="123456789";
+    @NotEmpty
+    @Size(min = 10)
+    private String description="submission";
+    @NotEmpty
+    private String locale="fr-BE";    
+}
--- src/main/java/com/pieterjd/barcode/barcode/BarcodeMvcController.java
+++ src/main/java/com/pieterjd/barcode/barcode/BarcodeMvcController.java
@@ -1,10 +1,18 @@
 package com.pieterjd.barcode.barcode;
 
+import java.util.Locale;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
+
 
 
 @Controller
@@ -22,5 +30,30 @@ public class BarcodeMvcController {
         model.addAttribute("barcodes", barcodeRepository.findAll(Pageable.unpaged()).getContent());
         return "barcodes";
     }
+
+    @GetMapping("/add")
+    public String barcodesSubmit(Model model){
+        model.addAttribute("submission", new AddBarcodeSubmission()) ;
+        return "barcodesAdd";
+    }
+
+    @PostMapping("/add")
+    public String postMethodName(@Valid @ModelAttribute("submission") AddBarcodeSubmission submission,BindingResult result,Model model) {
+        if(result.hasErrors()){
+            return "barcodesAdd";
+        }
+        Description description = Description.builder()
+            .locale(Locale.forLanguageTag(submission.getLocale()))
+            .text(submission.getDescription())
+            .build();
+        Barcode barcode = Barcode.builder()
+        .barcode(submission.getBarcode())
+        .build();
+        barcode.addDescription(description);
+
+        barcodeRepository.save(barcode);
+        return "redirect:/barcodes/add";
+    }
+    
     
 }
