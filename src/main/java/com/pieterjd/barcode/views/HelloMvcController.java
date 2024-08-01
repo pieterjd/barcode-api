7093df2224e959bc6f39f9fe19ea1dcd1e424481
Pieter-Jan Drouillon <drouillon@hotmail.com>
2024-08-01 11:52:11 +0200

    initial security config

---
+++ src/main/java/com/pieterjd/barcode/views/HelloMvcController.java
@@ -0,0 +1,17 @@
+package com.pieterjd.barcode.views;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/hello")
+public class HelloMvcController {
+    @GetMapping
+    public String getMethodName() {
+        return "hello";
+    }
+    
+    
+}
