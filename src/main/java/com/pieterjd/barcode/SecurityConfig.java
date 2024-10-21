9a1786a2b401f16a5fdc2c941400767703fcf02f
Pieter-Jan Drouillon <drouillon@hotmail.com>
2024-10-21 20:21:58 +0200

    use jdbcUserDetailManager

--- src/main/java/com/pieterjd/barcode/SecurityConfig.java
+++ src/main/java/com/pieterjd/barcode/SecurityConfig.java
@@ -1,43 +1,58 @@
 package com.pieterjd.barcode;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private DataSource datasource;
+
+    public SecurityConfig(DataSource datasource) {
+        this.datasource = datasource;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .userDetailsService(userManager())
                 .formLogin(Customizer.withDefaults())
-                .authorizeHttpRequests(ahr -> ahr.requestMatchers("/login","/hello").permitAll()
+                .authorizeHttpRequests(ahr -> ahr.requestMatchers("/login", "/hello", "/h2-console/**").permitAll()
                         .anyRequest().authenticated())
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")))
+                .headers(headers -> headers.frameOptions().disable())
                 .build();
     }
 
     @Bean
     public UserDetailsManager userManager() {
-        return new InMemoryUserDetailsManager(
-                new User("admin", "$2a$10$j37/78fF9Eld9UeoTSbafe0TlGFCx3/flDwoNVe6ByH8bG9T2yhs.", AuthorityUtils.createAuthorityList("USER", "ADMIN")),
-                new User("user", "$2a$10$zwtfASO2cW0Opqx.FXq.wui3eMgTDf.cjcvlLnUXBjTRh1E2q32eW", AuthorityUtils.createAuthorityList("USER")));
+        return new JdbcUserDetailsManager(datasource);
+        /*
+         * return new InMemoryUserDetailsManager(
+         * new User("admin",
+         * "$2a$10$j37/78fF9Eld9UeoTSbafe0TlGFCx3/flDwoNVe6ByH8bG9T2yhs.",
+         * AuthorityUtils.createAuthorityList("USER", "ADMIN")),
+         * new User("user",
+         * "$2a$10$zwtfASO2cW0Opqx.FXq.wui3eMgTDf.cjcvlLnUXBjTRh1E2q32eW",
+         * AuthorityUtils.createAuthorityList("USER")));
+         */
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
 
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -21,3 +21,21 @@ create table DESCRIPTION(
    locale varchar(10),
    "TEXT" varchar(255)
 );
+
+-- spring security tables
+create table if not exists users (
+	username varchar_ignorecase(50) not null primary key,
+	password varchar_ignorecase(500) not null,
+	enabled boolean not null
+);
+
+create table if not exists authorities (
+	username varchar_ignorecase(50) not null,
+	authority varchar_ignorecase(50) not null,
+	constraint fk_authorities_users foreign key(username) references users(username)
+);
+create unique index ix_auth_username on authorities (username,authority);
+
+insert into users values ('admin','$2a$10$j37/78fF9Eld9UeoTSbafe0TlGFCx3/flDwoNVe6ByH8bG9T2yhs.', true);
+INSERT INTO authorities (username, authority) VALUES ('admin', 'ROLE_USER');
+INSERT INTO authorities (username, authority) VALUES ('admin', 'ROLE_ADMIN');
